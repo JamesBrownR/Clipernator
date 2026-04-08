650c29dc413d6cc99a1e892d954031b57670db71
JamesBrownR <jamesbrown1real@gmail.com>
2026-04-07 23:30:05 -0400

    Update game.js

--- jsstuff/game.js
+++ jsstuff/game.js
@@ -115,7 +115,7 @@ function initGameState() {
     hasFunhouseDistortion: false,
     hasTightropeBoots: false,
     knockingPinsActive: true, knockingPinsTimer: 1,
-    hasClownish: false,
+    hasClownish: true,
     clownNoseSize: 0, clownNoseTimer: 0, clownNoseMax: 480,
     clownNoseHonkTimer: 0,
     clownConfuseActive: false,
